3333a8afa6420caa7d9cf7ba7736308c7d4c9106
Rod Vagg <rod@vagg.org>
2019-09-13 20:27:11 +1000

    (feat) `advanced` definitions and representations

--- print.js
+++ print.js
@@ -5,6 +5,27 @@ function print (schema, indent = '  ') {
 
   let str = ''
 
+  str += printAdvanced(schema, indent)
+  str += printTypes(schema, indent)
+
+  return str
+}
+
+function printAdvanced (schema, indent) {
+  let str = ''
+
+  if (typeof schema.advanced === 'object') {
+    for (const advanced of Object.keys(schema.advanced)) {
+      str += `advanced ${advanced}\n\n`
+    }
+  }
+
+  return str
+}
+
+function printTypes (schema, indent) {
+  let str = ''
+
   for (const [type, defn] of Object.entries(schema.types)) {
     if (typeof defn.kind !== 'string') {
       throw new Error(`Invalid schema ${type} doesn't have a kind`)
@@ -20,9 +41,15 @@ function printType (defn, indent) {
   if (['map', 'list', 'link'].includes(defn.kind)) {
     return printTypeTerm(defn, indent)
   }
+
   if (['struct', 'union', 'enum'].includes(defn.kind)) {
     return `${defn.kind} ${printTypeTerm(defn, indent)}`
   }
+
+  if (defn.kind === 'bytes' && defn.representation && typeof defn.representation.advanced === 'string') {
+    return `bytes representation advanced ${defn.representation.advanced}`
+  }
+
   return defn.kind
 }
 
@@ -32,7 +59,6 @@ function printTypeTerm (defn, indent) {
   }
 
   if (typeof printTypeTerm[defn.kind] !== 'function') {
-    console.log('blurp', defn)
     throw new Error(`Invalid schema unsupported kind (${defn.kind})`)
   }
 
@@ -58,6 +84,8 @@ printTypeTerm.map = function map (defn, indent) {
       str += ' representation listpairs'
     } else if (typeof defn.representation.stringpairs === 'object') {
       str += stringpairs(indent, 'map', defn.representation.stringpairs)
+    } else if (typeof defn.representation.advanced === 'string') {
+      str += ` representation advanced ${defn.representation.advanced}`
     }
   }
   return str
@@ -69,7 +97,15 @@ printTypeTerm.list = function list (defn) {
   }
 
   const nullable = defn.valueNullable === true ? 'nullable ' : ''
-  return `[${nullable}${printTypeTerm(defn.valueType)}]`
+  let str = `[${nullable}${printTypeTerm(defn.valueType)}]`
+
+  if (defn.representation) {
+    if (typeof defn.representation.advanced === 'string') {
+      str += ` representation advanced ${defn.representation.advanced}`
+    }
+  }
+
+  return str
 }
 
 printTypeTerm.struct = function struct (defn, indent) {
@@ -135,6 +171,8 @@ printTypeTerm.struct = function struct (defn, indent) {
       }
     } else if (typeof defn.representation.stringpairs === 'object') {
       str += stringpairs(indent, 'struct', defn.representation.stringpairs)
+    } else if (typeof defn.representation.advanced === 'string') {
+      str += ` representation advanced ${defn.representation.advanced}`
     }
   }
 
