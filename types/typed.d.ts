0b09c703e67824836c1c61a647f081ff75d4fb15
Rod Vagg <rod@vagg.org>
2022-08-19 16:07:57 +1000

    fix: make enums behave properly

--- types/typed.d.ts
+++
@@ -1,5 +0,0 @@
-import { Builder } from "./lib/typed-gen.js";
-import { create } from "./lib/typed-gen.js";
-import { safeReference } from "./lib/typed-gen.js";
-export { Builder, create, safeReference };
-//# sourceMappingURL=typed.d.ts.map
