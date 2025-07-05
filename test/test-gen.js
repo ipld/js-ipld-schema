78cfdcfe5feb9462ca00878192526ba205d886d2
Rod Vagg <rod@vagg.org>
2025-07-05 20:22:29 +1000

    feat(gen): maps

--- test/test-gen.js
+++ test/test-gen.js
@@ -94,3 +94,32 @@ describe('Generate list types', () => {
     assert.strictEqual(ts, expectedTypeScript)
   })
 })
+
+describe('Generate map types', () => {
+  let schema, expectedGo, expectedRust, expectedTypeScript
+
+  before(async () => {
+    const contents = await readFile(new URL('./fixtures/gen/maps.md', import.meta.url), 'utf8')
+    const dirEnt = index(parse(contents))
+    const schemaText = dirEnt.children.get('test').children.get('schema').hunk.body
+    schema = fromDSL(schemaText, { includeComments: true, includeAnnotations: true })
+    expectedGo = dirEnt.children.get('test').children.get('golang').hunk.body
+    expectedRust = dirEnt.children.get('test').children.get('rust').hunk.body
+    expectedTypeScript = dirEnt.children.get('test').children.get('typescript').hunk.body
+  })
+
+  it('Go', async () => {
+    const go = generateGo(schema, { package: 'main' })
+    assert.strictEqual(go, expectedGo)
+  })
+
+  it('Rust', async () => {
+    const rust = generateRust(schema)
+    assert.strictEqual(rust, expectedRust)
+  })
+
+  it('TypeScript', async () => {
+    const ts = generateTypeScript(schema)
+    assert.strictEqual(ts, expectedTypeScript)
+  })
+})
